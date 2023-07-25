32f7d96d9d5fd34b700d5b33337cecaac729aee0
Fernando Linhares <falinhares2001@hotmail.com>
2023-07-24 22:34:46 -0300

    Fim do project 1

--- bs5/projects/ebook-website-starter/js/script.js
+++ bs5/projects/ebook-website-starter/js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+//Fazer a navbar ficar escura com o scroll
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('bg-dark');
+            navbar.classList.add('navbar-sticky');
+        } else {
+            navbar.classList.remove('bg-dark');
+            navbar.classList.remove('navbar-sticky');
+        }
+    });
+}
+
+//Ativa a função ao carregar a tela
+document.addEventListener('DOMContentLoaded', userScroll);
